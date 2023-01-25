a35c2f3d948bbb9d0bc9cb5de3c65e5a513441e0
Silvio Arem <silvio.arem@gmail.com>
2023-01-25 16:59:16 -0300

    tentando redefinir a porta para API

--- src/Pages/Psicologo/list.tsx
+++ src/Pages/Psicologo/list.tsx
@@ -1,9 +1,35 @@
-export default function PsicologoList () {
+import { useEffect, useState } from "react";
+import { listarPsicologo } from "../../services/MainApi/psicologos";
+
+interface Psicologo{
+    nome: string;
+    apresentacao: string;
+    email: string;
+    senha: string;
+}
+
+export default function PsicologoList() {
+    const [psicologos, setPsicologos] = useState<Psicologo[]>([]);
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const response = await listarPsicologo();
+
+                setPsicologos(response.data);
+            } catch (error) {
+                alert("Deu algo errado");
+            }
+        };
+
+        getData();
+    }, [setPsicologos]);
     return (
         <main className="container card my-5 p-5">
             <h1>Lista de Psicologo</h1>
             <ul className="list-group">
-                <li className="list-group-item">Psicologo 1</li>
+                {psicologos.map(psicologo => (
+                    <li className="list-group-item">{psicologo.nome}</li>
+                ))}
             </ul>
         </main>
     );
--- src/services/MainApi/config/index.ts
+++ src/services/MainApi/config/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseApi = axios.create({
-    baseURL: "http://localhost:3306",
+    baseURL: "http://localhost:3000",
     headers: {
         "Content-Type": "application/json",
     }
