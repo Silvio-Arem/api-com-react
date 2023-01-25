a031199395962296174e42e4d7b427768b99cfd5
Silvio Arem <silvio.arem@gmail.com>
2023-01-25 13:38:28 -0300

    formulario pronto

--- src/Pages/Psicologo/create.tsx
+++ src/Pages/Psicologo/create.tsx
@@ -8,7 +8,46 @@ export default function PsicologoCreate() {
 
     return (
         <main className="container card my-5 p-5">
-            
+            <h1>Cadastro de Psicologo</h1>
+            <form action="">
+                <div className="mb-3">
+                    <label className="form-label">Nome</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={nome}
+                        onChange={(event) => setNome(event.target.value)}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Apresentação</label>
+                    <textarea
+                        className="form-control"
+                        value={apresentacao}
+                        onChange={(event) => setNome(event.target.value)}
+                    ></textarea>
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Email</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        value={email}
+                        onChange={(event) => setNome(event.target.value)}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Senha</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        value={senha}
+                        onChange={(event) => setNome(event.target.value)}
+                    />
+                </div>
+
+                <button className="btn btn-primary">Cadastrar</button>
+            </form>
         </main>
     )
 }
